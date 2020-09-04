884c18566b83de00f72a80d1c339895ea7decbc2
Abhinav <tiwariabhinavin@gmail.com>
2020-09-04 22:37:22 +0530

    Added form in contact

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class contact(models.Model):
+    sno=models.AutoField(primary_key=True);
+    name=models.CharField(max_length=30);
+    email=models.EmailField();
+    phone=models.IntegerField();
+    content=models.TextField();
+    timestamp=models.DateTimeField(auto_now_add=True,blank=True)
